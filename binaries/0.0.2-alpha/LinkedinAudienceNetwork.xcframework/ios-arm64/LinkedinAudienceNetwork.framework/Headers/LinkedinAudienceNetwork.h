38b5501618cfc78fc4c17839874d11586c59a27e
Guanyu He <ghe@linkedin.com>
2022-05-24 11:58:22 -0700

    add binaries directory to avoid void

---
+++ binaries/0.0.2-alpha/LinkedinAudienceNetwork.xcframework/ios-arm64/LinkedinAudienceNetwork.framework/Headers/LinkedinAudienceNetwork.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for LinkedinAudienceNetwork.
+FOUNDATION_EXPORT double LinkedinAudienceNetworkVersionNumber;
+
+//! Project version string for LinkedinAudienceNetwork.
+FOUNDATION_EXPORT const unsigned char LinkedinAudienceNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinkedinAudienceNetwork/PublicHeader.h>
+
+
